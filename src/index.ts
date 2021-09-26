a9b960b91e8372994d0b2ce4c7ec215725da6e09
Danilo Chagas <83218983+danilo-ac@users.noreply.github.com>
2021-09-26 19:34:06 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use("/user", userRouter);
 app.use("/show", showRouter)
 app.use("/band",bandRouter)
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
     if (server) {
       const address = server.address() as AddressInfo;
       console.log(`Servidor rodando em http://localhost:${address.port}`);
